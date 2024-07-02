1311ffa12a16a69bb3587d9933daea4e99660677
niknak11 <niknak1188@gmail.com>
2024-07-02 16:31:15 -0700

    fix to app.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const config = require('./config/config');
+const path = require('path');
 require('express-async-errors');
 
 const app = express();
