ac1e6fa50b8f819050206cd8e1a8da8e09c1eaa2
niknak11 <niknak1188@gmail.com>
2024-07-02 12:59:51 -0700

    updated backend URL proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,10 +7,12 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://cabo-e6au.onrender.com',
         changeOrigin: true,
       },
     },
     historyApiFallback: true,
   },
 });
+
+//http://localhost:3001
