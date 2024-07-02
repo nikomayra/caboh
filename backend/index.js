e9698e4030f0fd728ca341f27f955f84d4c0f038
niknak11 <niknak1188@gmail.com>
2024-07-02 11:39:30 -0700

    added default port for index.js if env not read

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const app = require('./app');
 const config = require('./config/config');
 const logger = require('./utils/logger');
 
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`);
+const PORT = config.PORT || 3001;
+app.listen(PORT, () => {
+  logger.info(`Server running on port ${PORT}`);
 });
